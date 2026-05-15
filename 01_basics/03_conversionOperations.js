a7963d788591b7f1a9f42cee9752c9c4e870a9f5
Shaban Ali <shabi9405@gmail.com>
2026-05-15 13:18:51 +0000

    Conversions of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,45 @@
+let score = "shaban"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let ValueInNumber = Number(score)
+
+console.log(typeof ValueInNumber)
+console.log(ValueInNumber)
+
+
+// "33" => 33 
+// "33abc" => NaN
+// true => 1 , false => 0 
+
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+
+// let isLoggedIn = ""
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+
+let isLoggedIn = "Shaban"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+// 1=> true , 0 => false 
+// "" => false 
+// "shaban" => true 
+
+let someNumber = 512
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
