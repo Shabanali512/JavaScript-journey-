29d42c025a07dbbce6747302a297ee4872865fbe
Shaban Ali <shabi9405@gmail.com>
2026-05-15 12:45:24 +0000

    Datatypes in JavaScript

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version 
+
+// alert(3+3)  // we are using nodejs, not browser 
+
+console.log(3
+    +3
+)  // code readability should be high
+
+let accountCity = "null"
+
+// JavaScript ke 7 Primitive Data Types
+
+// number => 2 to power 53 
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined => agr value assigned he na di ho sirf variable bna ho 
+// symbol => unique hota h 
+
+
+console.log(accountCity)
+
+// typeof => batata hai ke variable ka data type kya hai
+
+console.log(typeof null) // output: "object" (yeh JS ki purani galti hai, fix nahi hui)
+console.log(typeof undefined) // undefined he output ata h 
