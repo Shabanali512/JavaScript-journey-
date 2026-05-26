0700ac39981764eb930286781b0f9b716f82adfc
Shaban Ali <shabi9405@gmail.com>
2026-05-26 13:20:19 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,18 @@
+const month = 3 
+
+switch (month){
+
+    case 1: 
+            console.log("January")
+            break ; 
+
+    case 2: 
+            console.log("Febraury")
+            break ; 
+    case 3: 
+            console.log("March")
+            break ; 
+    default :
+            console.log("Default")
+            break ;
+}
