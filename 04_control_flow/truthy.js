87695ccf41b38fa0b08fe679949417733f93ef8e
Shaban Ali <shabi9405@gmail.com>
2026-05-26 14:45:10 +0000

    truthy

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,31 @@
+// const UserEmail = "Shaban@gmail.com"
+
+// if(UserEmail){
+//     console.log("got user email")
+// }
+// else {
+//     console.log("don't have user email")
+// }
+
+// const UserEmail = ""
+
+// if(UserEmail){
+//     console.log("got user email")
+// }
+// else {
+//     console.log("don't have user email")
+// }
+
+const UserEmail = []
+
+if(UserEmail){
+    console.log("got user email")
+}
+else {
+    console.log("don't have user email")
+}
+
+
+// falsy values 
+
+// false , 0 ,-0 , bigInt 0n , null , undefined , "NaN" 
