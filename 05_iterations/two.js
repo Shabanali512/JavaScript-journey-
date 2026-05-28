d764a20f6e84f85457263df5e89d0c12bda15b6a
Shaban Ali <shabi9405@gmail.com>
2026-05-28 16:46:54 +0000

    for loop,  while loop , do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,27 @@
+// while loop 
+
+// let index = 0 ;
+
+// while (index <= 10) { 
+//     console.log(`value of index is ${index}`);
+//     index = index + 2 
+    
+    
+// }
+
+let myArray = ["Shaban" , "Ali" , "Ahmed"]
+
+let arr = 0 ; 
+
+while (arr < myArray.length ) {
+
+    console.log(`Name is ${myArray[arr]}`);
+    arr = arr + 1
+    
+}
+
+let score = 11
+
+do {
+    console.log(`score is ${score}`)
+}while(score <= 10)
