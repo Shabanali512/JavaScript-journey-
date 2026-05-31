47fe78e6d7dbc4e89f3e03326259d3a5c150c59f
Shaban Ali <shabi9405@gmail.com>
2026-05-31 09:59:18 +0000

    for in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,34 @@
+const myObject = {
+
+    js : 'Javascript',
+    cpp : 'C++' , 
+    py : 'Python ' , 
+    rb : 'Ruby'
+} 
+
+for(const key in myObject){
+
+    // console.log(myObject[key])
+} 
+
+const programing = ["js" , "py" , "cpp" , "java"]
+
+for(const key in programing){
+
+    console.log(programing[key]);
+    
+}
+
+
+// const map = new Map() 
+// map.set('PK' , "Pakistan")
+// map.set('USA', "United States of America" )
+// map.set('Uk' , " United kingdom")
+
+// for(const key in map){
+//     console.log(map)
+
+// } 
+
+// map is not iterateable in For in loop  
+
